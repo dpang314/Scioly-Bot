86572fadf1d81b9070a5397f4a03fcb15130c0eb
David Pang <davidpang314@gmail.com>
2022-02-01 22:29:50 -0500

    feat: Navbar links and login work

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -3,17 +3,31 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-  
-export default function Navbar() {
+import Link from 'next/link';
+import { FunctionComponent } from 'react';
+import { signIn, signOut } from 'next-auth/react'
+
+type NavbarProps = {
+  loggedIn: boolean,
+  page: string,
+}
+
+const Navbar: FunctionComponent<NavbarProps> = ({ loggedIn, page }) => {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit">Tournaments</Button>
-          <Button color="inherit">Templates</Button>
-          <Button color="inherit">Login</Button>
+          <Link href={'tournaments'}><Button color="inherit">Tournaments</Button></Link>
+          <Link href={'templates'}><Button color="inherit" >Templates</Button></Link>
+          <div style={{flex: 1}}></div>
+          {
+            loggedIn ? <Button color="error" variant="outlined" onClick={() => signOut()}>Log Out</Button>
+            : <Button color="success" variant="contained" onClick={() => signIn()}>Log In</Button>
+          }
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
+export default Navbar;

fab80d3d598eccbc50fe1ed19826a9da9a4cd117
David Pang <davidpang314@gmail.com>
2022-02-07 21:47:10 -0500

    fix: Change template routes to use new models.
    - POST create with template events
    
    - GET populate with template events
    
    - Protect route

--- src/pages/api/templates.ts
+++ src/pages/api/templates.ts
@@ -1,15 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Template } from '../../models';
+import { getSession } from 'next-auth/react';
+import { Template, TemplateEvent } from '../../models';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === 'POST') {
-    const template = await Template.create(req.body);
-    res.status(200).json(template);
-  } else if (req.method === 'GET') {
-    const templates = await Template.findAll({});
-    res.status(200).json(templates);
+  const session = await getSession({ req });
+  if (session) {
+    if (req.method === 'POST') {
+      const template = await Template.create(req.body, {
+        include: [{
+          model: TemplateEvent,
+          as: "templateEvents",
+        }]
+      });
+      res.status(200).json(template);
+    } else if (req.method === 'GET') {
+      const templates = await Template.findAll({ include: [{ model: TemplateEvent, as: "templateEvents" }] });
+      res.status(200).json(templates);
+    }
+  } else {
+    res.status(401).end();
   }
 }

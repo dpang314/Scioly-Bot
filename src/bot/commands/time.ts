6e589ebf7025fff3ae963fdcbc36db3bfc6bb530
David Pang <davidpang314@gmail.com>
2022-02-08 22:22:41 -0500

    refactor: Change time bot command to use ORM

--- src/bot/commands/time.ts
+++ src/bot/commands/time.ts
@@ -1,35 +1,25 @@
 import { CommandInteraction, MessageEmbed } from "discord.js";
 
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { TEST_LENGTH, TEST_OPTIONS } from "../../configLoader";
-import { Test } from "../../models";
-
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.tz.setDefault('America/New_York');
+import { Test, TournamentEvent } from "../../models";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('time')
     .setDescription('Sends time left on current test(s).'),
   async execute(interaction: CommandInteraction) {
-    const tests = await Test.findAll({ where: { user_id: interaction.user.id } });
+    const tests = await Test.findAll({ where: { userId: interaction.user.id, finished: false }, include: [ { model: TournamentEvent, as: 'tournamentEvent' } ] });
+    tests.concat(await Test.findAll({ where: { partner1Id: interaction.user.id, finished: false }, include: [ { model: TournamentEvent, as: 'tournamentEvent' } ] }));
+    tests.concat(await Test.findAll({ where: { partner2Id: interaction.user.id, finished: false }, include: [ { model: TournamentEvent, as: 'tournamentEvent' } ] }));
     const testsEmbed = new MessageEmbed()
-    .setColor('RANDOM')
-    //.setColor('#0099ff')
-    .setTitle('Current tests');
+      .setColor('RANDOM')
+      .setTitle('Current tests');
     for (const test of tests) {
-      const currentTime = dayjs();
-      const testStart = dayjs(test._attributes.time_started);
-      const minuteDifference = currentTime.diff(testStart) / 1000 / 60;
-      if (!test._attributes.finished) {
-        const testLabel = TEST_OPTIONS.find(element => element.value === test._attributes.test_name)?.label;
-        if (testLabel) {
-          testsEmbed.addField(testLabel, `${(TEST_LENGTH - minuteDifference).toFixed(2)} minutes`);
-        }
+      if (test) {
+        const currentTime = new Date().getTime();
+        const testStart = test.timeStarted.getTime();
+        const minuteDifference = (currentTime - testStart) / 1000.0 / 60;
+        testsEmbed.addField(test.tournamentEvent.name, `${(test.tournamentEvent.minutes - minuteDifference).toFixed(2)} minutes`);
       }
     }
     await interaction.reply({ embeds: [testsEmbed] });

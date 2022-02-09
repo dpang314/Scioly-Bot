82160a4c8a081f4c2b86af72ca5bec802bddff35
David Pang <davidpang314@gmail.com>
2022-02-08 22:23:40 -0500

    refactor: Change checkTests task to use ORM

--- src/bot/tasks/checkTests.ts
+++ src/bot/tasks/checkTests.ts
@@ -1,26 +1,19 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
-import { Test } from "../../models";
+import { Test, Tournament, TournamentEvent } from "../../models";
 import { client } from '../index';
 import { MessageEmbed, User } from 'discord.js';
-import { SUBMISSION_LINK, TEST_LENGTH } from '../../configLoader';
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.tz.setDefault('America/New_York');
 
 const checkTests = async() => {
-  const sendReminder = async(user: User | undefined, id: string | undefined) => {
+  const sendReminder = async(user: User | undefined, test: Test, event: TournamentEvent, tournament: Tournament) => {
     try {
-      if (user && id) {
+      if (user && test) {
         const reminderEmbed = new MessageEmbed()
           .setColor('RANDOM')
-          //.setColor('#0099ff')
           .setTitle('TIME IS UP! SUBMIT NOW!')
-          .setURL(SUBMISSION_LINK)
+          .setURL(tournament.submission)
           .addFields(
-            { name: 'Private test ID', value: `${id}` },
-            { name: 'Submission link', value: `[${SUBMISSION_LINK}](${SUBMISSION_LINK})`}
+            { name: 'Test', value: event.name },
+            { name: 'Private test ID', value: `${test.id}` },
+            { name: 'Submission link', value: `[${tournament.submission}](${tournament.submission})`},
           );
 
         const DMChannel = await user.createDM();
@@ -32,23 +25,35 @@ const checkTests = async() => {
       console.log('Something went wrong.')
     }
   }
-  
-  const tests = await Test.findAll({ where: { finished: false } });
-  for (const test of tests) {
-    const currentTime = dayjs();
-    const testStart = dayjs(test._attributes.time_started);
-    const minuteDifference = currentTime.diff(testStart) / 1000 / 60;
-    if (minuteDifference >= TEST_LENGTH && minuteDifference < TEST_LENGTH + 1) {
-      await Test.update({ finished: true }, { where: { id: test._attributes.id } });
-      const user = await client.users.fetch(test._attributes.user_id);
-      sendReminder(user, test._attributes.id);
-      if (test._attributes.partner1_id) {
-        const partner1 = await client.users.fetch(test._attributes.partner1_id)
-        sendReminder(partner1, test._attributes.id);
-      }
-      if (test._attributes.partner2_id) {
-        const partner2 = await client.users.fetch(test._attributes.partner2_id)
-        sendReminder(partner2, test._attributes.id);
+
+  const tournaments = await Tournament.findAll({ include: [ { model: TournamentEvent, as: "tournamentEvents", include: [ {model: Test, as: "tests"} ] } ] })
+  const notArray = (array: Array<unknown>) => {
+    return !Array.isArray(array) || !array.length;
+  }
+  if (notArray(tournaments)) return;
+  for (const tournament of tournaments) {
+    if (notArray(tournament.tournamentEvents)) continue;
+    for (const event of tournament.tournamentEvents) {
+      if (notArray(event.tests)) continue;
+      for (const test of event.tests) {
+        if (!test.finished) {
+          const currentTime = new Date().getTime();
+          const testStart = test.timeStarted.getTime();
+          const minuteDifference = (currentTime - testStart) / 1000.0 / 60;
+          // If time isn't up
+          if (minuteDifference < event.minutes) continue;
+          await test.update({ finished: true });
+          const user = await client.users.fetch(test.userId);
+          await sendReminder(user, test, event, tournament);
+          if (test.partner1Id) {
+            const partner1 = await client.users.fetch(test.partner1Id)
+            await sendReminder(partner1, test, event, tournament);
+          }
+          if (test.partner2Id) {
+            const partner2 = await client.users.fetch(test.partner2Id)
+            await sendReminder(partner2, test, event, tournament);
+          }
+        }
       }
     }
   }

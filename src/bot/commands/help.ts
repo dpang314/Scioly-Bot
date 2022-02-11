e09320e466135ae3ea92475148e97ccbfd99b0e1
dpang314 <davidpang314@gmail.com>
2022-02-10 19:13:13 -0500

    fix: Removed unnecessary environment variables

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -1,13 +1,13 @@
 import { CommandInteraction } from 'discord.js';
 
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { GUIDELINES_LINK } from '../../configLoader';
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
     .setDescription('Sends practice tournament guidelines document'),
   async execute(interaction: CommandInteraction) {
-    await interaction.reply(`${GUIDELINES_LINK}`);
+    // TODO create more descriptive help command
+    await interaction.reply('Run /test or /time');
   },
 };
--- src/configLoader.ts
+++ src/configLoader.ts
@@ -16,10 +16,6 @@ const DISCORD_TOKEN = getEnv('DISCORD_TOKEN');
 const CLIENT_ID = getEnv('CLIENT_ID');
 const DISCORD_SECRET = getEnv('DISCORD_SECRET');
 const GUILD_ID = getEnv('GUILD_ID');
-const CHANNEL_ID = getEnv('CHANNEL_ID');
-const MONGO_URI = getEnv('MONGO_URI');
-const SUBMISSION_LINK = getEnv('SUBMISSION_LINK');
-const GUIDELINES_LINK = getEnv('GUIDELINES_LINK');
 const DATABASE_CONNECTION = getEnv('DATABASE_CONNECTION');
 const DEV = process.env.NODE_ENV !== 'production';
 const SERVER = process.env.NODE_ENV !== 'production' ? 'http://localhost' : '';
@@ -31,10 +27,6 @@ export {
   CLIENT_ID,
   DISCORD_SECRET,
   GUILD_ID,
-  CHANNEL_ID,
-  MONGO_URI,
-  SUBMISSION_LINK,
-  GUIDELINES_LINK,
   DATABASE_CONNECTION,
   DEV,
   SERVER,

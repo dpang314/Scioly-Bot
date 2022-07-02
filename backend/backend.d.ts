8c17a5e174b7fe3fc04414a2b875277f34dbfb66
David Pang <davidpang314@gmail.com>
2022-07-02 18:47:19 -0400

    misc: configure backend TypeScript

--- backend/backend.d.ts
+++ backend/backend.d.ts
@@ -1,10 +1,8 @@
-export {}
-
-declare global {
-  namespace Express {
-    interface User {
-      id: string;
-      discordName: string;
-    }
+declare namespace Express {
+  export interface User {
+    id: string;
+  }
+  export interface Request {
+    user?: User | undefined;
   }
 }

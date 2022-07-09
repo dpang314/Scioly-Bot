636e6a36a4b3071d6a8244bc63332af681b1b487
David Pang <davidpang314@gmail.com>
2022-07-08 22:25:00 -0400

    fix: remove unnecessary routes

--- packages/backend/src/routes/api/index.ts
+++ packages/backend/src/routes/api/index.ts
@@ -1,14 +1,10 @@
 import {Router} from 'express';
-import templateEventsRouter from './templateEvents';
 import templatesRouter from './templates';
-import tournamentEventsRouter from './tournamentEvents';
 import tournamentsRouter from './tournaments';
 
 const apiRouter = Router();
 
 apiRouter.use('/templates', templatesRouter);
-apiRouter.use('/templates', templateEventsRouter);
 apiRouter.use('/tournaments', tournamentsRouter);
-apiRouter.use('/tournaments', tournamentEventsRouter);
 
 export default apiRouter;

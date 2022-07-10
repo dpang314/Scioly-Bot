c33432e62220ec581340eced5edba43642de7c90
David Pang <davidpang314@gmail.com>
2022-07-09 21:01:05 -0400

    test: remove unnecessary Cypress files

--- cypress/integration/unauthorized.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Unauthorized', () => {
-  it('Unauthorized page can be opened', () => {
-    cy.visit('http://localhost:3000/unauthorized');
-    cy.contains(
-      'Unauthorized. Access is restricted to users with admin permission on the Discord server specified in the site configuration.',
-    );
-  });
-});
